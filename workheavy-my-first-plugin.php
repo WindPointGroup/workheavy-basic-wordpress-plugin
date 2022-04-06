c13a209090023ea6da9b42f0888148b0e7c27b15
John Binzak <john.binzak@gmail.com>
2022-04-06 13:25:50 -0500

    init plugin

---
+++ workheavy-my-first-plugin.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: WorkHeavy My First Plugin.
+description: A demo WP plugin to help with learn development.
+Version: 1.0
+Author: WorkHevy
+Author URI: https://workheavy.com
+*/
